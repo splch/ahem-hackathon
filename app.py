2ac87e22745e5f3893671465f8df7a533d094d1e
Moses <moses@genieapp.net>
2020-12-10 13:24:28 -0800

    final commit

--- app.py
+++ app.py
@@ -4,21 +4,23 @@ import base64
 from functools import partial
 from pathlib import Path
 from multiprocessing import Pool
+from matplotlib import pyplot as plt
 import os
 import shutil
 import numpy as np
 import pandas as pd
 import librosa
+import soundfile as sf
 from scipy.io import wavfile
 from tqdm import tqdm_notebook as tqdm
+from random import randrange
 import torch.nn.functional as F
-#from fastai.basic_data import DatasetType
-
+# from fastai.basic_data import DatasetType
 import fastai
 from fastai.vision import *
 
+counter = 1
 app = Flask(__name__)
-
 
 
 def read_file(filename, path='', sample_rate=None, trim=False):
@@ -31,35 +33,38 @@ def read_file(filename, path='', sample_rate=None, trim=False):
         raise OSError('Encounted unexpected dtype: {}'.format(data.dtype))
     if sample_rate is not None and sample_rate != file_sr:
         if len(data) > 0:
-            data = librosa.core.resample(data, file_sr, sample_rate, res_type='kaiser_fast')
+            data = librosa.core.resample(
+                data, file_sr, sample_rate, res_type='kaiser_fast')
         file_sr = sample_rate
     if trim and len(data) > 1:
         data = librosa.effects.trim(data, top_db=40)[0]
     return data, file_sr
 
+
 def log_mel_spec_tfm(fname, src_path, dst_path):
     x, sample_rate = read_file(fname, src_path)
-    
+
     n_fft = 1024
     hop_length = 256
     n_mels = 40
     fmin = 20
-    fmax = sample_rate / 2 
-    
-    mel_spec_power = librosa.feature.melspectrogram(x, sr=sample_rate, n_fft=n_fft, 
-                                                    hop_length=hop_length, 
-                                                    n_mels=n_mels, power=2.0, 
+    fmax = sample_rate / 2
+
+    mel_spec_power = librosa.feature.melspectrogram(x, sr=sample_rate, n_fft=n_fft,
+                                                    hop_length=hop_length,
+                                                    n_mels=n_mels, power=2.0,
                                                     fmin=fmin, fmax=fmax)
     mel_spec_db = librosa.power_to_db(mel_spec_power, ref=np.max)
-    dst_fname = dst_path / (fname[:-4] + '.png')
+    dst_fname = dst_path + '/' + fname[:-4] + '.png'
     plt.imsave(dst_fname, mel_spec_db)
+
 
 def get_res():
     '''file: audio file
         returns percentage of COVID certainty [0,1]
     '''
 
-    x,_ = librosa.load('audio.wav', sr=16000)
+    x, _ = librosa.load('audio.wav', sr=16000)
     sf.write('tmp.wav', x, 16000)
 
     fn = 'tmp.wav'
@@ -68,28 +73,30 @@ def get_res():
 
     img = plt.imread(fn[:-4] + '.png')
 
-    learn = load_learner('learner.pkl')
-    learn.predict(open_image(fn[:-4] + '.png'))
-
-    return str(tuple(learn.predict(open_image(fn[:-4] + '.png'))[2].detach().cpu().numpy())[1])
-
+    learn = load_learner('')
+    learn_predict = learn.predict(open_image(fn[:-4] + '.png'))
+    print(learn_predict)
+    return tuple(learn_predict[2].detach().cpu().numpy())[1]
+            
+def get_rand(isHigh):
+    if isHigh:
+        return randrange(5,10) / 10
+    else:
+        return randrange(3) / 10
 
 @app.route('/post', methods=['GET', 'POST'])
 def get_audio():
     '''gets audio file as POST request from frontend'''
     if request.method == 'POST':
-        try:
-            form = request.form
-            file = form['file']
-            wav_file = open("audio.wav", "wb") # get audio file as base64
-            file = file[35:]
-            decode_string = base64.b64decode(file) # convert base64 to wav
-            wav_file.write(decode_string)
-            print("SUCCESS")
-            return get_res()
-        except Exception as e:
-            print("EXCEPTION: " + str(e))
-            return "-1"
+        form = request.form
+        file = form['file']
+        wav_file = open("audio.wav", "wb")  # get audio file as base64
+        file = file[35:]
+        decode_string = base64.b64decode(file)  # convert base64 to wav
+        wav_file.write(decode_string)
+        res = get_res()
+        print(res)
+        return str(res)
     else:
         return "GET"
 
