9d6f72e70c47f55cdb21f5b0fad3742c4cf65050
Moses <moses@genieapp.net>
2020-12-09 22:07:08 -0800

    audio upload

--- app.py
+++ app.py
@@ -1,18 +1,39 @@
-from flask import Flask
+from flask import Flask, request
+import base64
 app = Flask(__name__)
 
-def get_res(file):
-    return True
 
-@app.route('/post/audio', methods=['GET','POST'])
+def get_res():
+    '''file: audio file
+        returns percentage of COVID certainty [0,1]
+    '''
+    return perc
+
+
+@app.route('/post', methods=['GET', 'POST'])
 def get_audio():
+    '''gets audio file as POST request from frontend'''
     if request.method == 'POST':
-        file = request.files['cough_file']
-        return get_res(file)
+        try:
+            form = request.form
+            file = form['file']
+            wav_file = open("audio.wav", "wb") # get audio file as base64
+            file = file[35:]
+            decode_string = base64.b64decode(file) # convert base64 to wav
+            wav_file.write(decode_string)
+            print("SUCCESS")
+            return get_res()
+        except Exception as e:
+            print("EXCEPTION: " + str(e))
+            return "-1"
+    else:
+        return "GET"
+
 
 @app.route('/', methods=['GET'])
 def main():
     return "Hello World!"
+
 
 if __name__ == '__main__':
     app.run()
